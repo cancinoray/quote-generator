56992b4d540b33cb792fd2e90390b9a11ed4ca61
Ray Cancino <cancinoray@gmail.com>
2022-03-20 10:13:22 +0800

    Fix the loader

--- script.js
+++ script.js
@@ -3,10 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+//Loadin Spinner Shown
+function loading (){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Remove Loading Spinner Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote From API
 async function getQuote(){
+    loading();
+    //We need to use a Proxy URL to make our API call in order to avoid a CORS error
     const proxyUrl = 'https://fathomless-plateau-97382.herokuapp.com/'; //created a proxy in heroku
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
@@ -27,8 +43,11 @@ async function getQuote(){
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
+
+        //Stop Loader, Show Quote
+        complete();
     } catch(error){
-        console.log('wooops, no quote' , error);
+        getQuote();
     }
 
 }
@@ -38,7 +57,7 @@ async function getQuote(){
 function tweetQuote(){
     const quote = quoteText.innerText;
     const author = authorText.innerText;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote}-${author}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
     window.open(twitterUrl, '_blank');
 }
 
@@ -48,6 +67,5 @@ twitterBtn.addEventListener('click' , tweetQuote);
 
 // On Load
 getQuote();
-
 
 
